232333d46d57814d30f841a906df5d33fe14a754
Francisco Suntaxi <sfsuntaxi1@espe.edu.ec>
2024-03-11 17:49:08 -0500

    Crear rutas iniciales

--- sistema_academico_frontend/src/app/app-routing.module.ts
+++ sistema_academico_frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  { path: 'app', component: LayoutComponent, loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
