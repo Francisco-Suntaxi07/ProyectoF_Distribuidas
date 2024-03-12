6ae9659d22e867b8663e7763ef69e0a2debdccdb
Francisco Suntaxi <sfsuntaxi1@espe.edu.ec>
2024-03-12 18:56:45 -0500

    actualizar script de cursos

--- Scripts_Bdd/RegistrosBDD.sql
+++ Scripts_Bdd/RegistrosBDD.sql
@@ -7,14 +7,21 @@ DELETE FROM USUARIO;
 -- Ejemplo de inserción de un nuevo usuario
 
 BEGIN;
-SELECT insertar_usuario('STALYN FRANCISCO SUNTAXI LEMA', 'sfsuntaxi1', 'admin1', 'sfsuntaxi1@espe.edu.ec', 'ADMINISTRADOR');
+SELECT insertar_usuario('STALYN FRANCISCO SUNTAXI LEMA', 'sfsuntaxi1', 'admin', 'sfsuntaxi1@espe.edu.ec', 'ESTUDIANTE');
 SELECT insertar_usuario('MIREYA', 'mireya', '123', 'mireya@espe.edu.ec', 'PROFESOR');
 SELECT insertar_usuario('DARIO JAVIER MORALES CAIZA', 'djmorales', '321', 'djmorales@espe.edu.ec', 'PROFESOR');
+SELECT insertar_usuario('KLEBER AUGUSTO AGUILAR LEMA', 'kaaguolar', '321', 'kaaguolar@espe.edu.ec', 'PROFESOR');
+SELECT insertar_usuario('JUAN CARLOS TANDAZO CANDO', 'jctandazo', '321', 'jctandazo@espe.edu.ec', 'PROFESOR');
+SELECT insertar_usuario('SANG GUUN YOO', 'sgyoo', '12345678', 'sgyoo@espe.edu.ec', 'PROFESOR');
+SELECT insertar_usuario('ANGEL GEOVANNY CUDCO POMAGUALLI', 'agcudco', '87654321', 'agcudco@espe.edu.ec', 'PROFESOR');
 SELECT insertar_usuario('LUIS XAVIER ESPINOSA MEZA', 'lxespinosa', '123', 'lxespinosa@espe.edu.ec', 'ESTUDIANTE');
+SELECT insertar_usuario('SANTIAGO XAJIER RISUEÑO TABOADA', 'sxrisueño', '123', 'sxrisueño@espe.edu.ec', 'ESTUDIANTE');
 SELECT insertar_usuario('RICARDO SEBASTIAN GRIJALVA MOREJON', 'rsgrijalva', '123', 'rsgrijalva@espe.edu.ec', 'ESTUDIANTE');
-SELECT insertar_usuario('ALEJANDRO JOSUE TOSCANO TABOADA', 'ajtoscano', '123', 'ajtoscano@espe.edu.ec', 'ESTUDIANTE');
+SELECT insertar_usuario('ALEJANDRO JOSUE TOSCANO TABOADA', 'ajtoscano', 'clave', 'ajtoscano@espe.edu.ec', 'ESTUDIANTE');
+SELECT insertar_usuario('KARLA LIZBETH CAJAS CAJAS', 'klcajas', '123', 'kccajas@espe.edu.ec', 'ESTUDIANTE');
+SELECT insertar_usuario('ALISSON ALEJANDRA CAIZA CAIZA', 'aacaiz', 'clave', 'aacaiz@espe.edu.ec', 'ESTUDIANTE');
+SELECT insertar_usuario('CAMILA PACHECO PACHECO', 'ccpacheco', '123', 'ccpacheco@espe.edu.ec', 'ESTUDIANTE');
 COMMIT;
-
 
 select * from usuario;
 
@@ -29,8 +36,13 @@ VALUES
     ('15360', 'ASEG DE LA CALIDAD DE SOFTWARE', 25),
     ('15589', 'DESARROLLO DE SOFTWARE SEGURO', 20),
     ('15591', 'DESARROLLO DE VIDEO JUEGOS', 25),
-    ('15593', 'ING. SEGURIDAD DEL SOFTWARE', 20);
-
+    ('15593', 'ING. SEGURIDAD DEL SOFTWARE', 20),
+	('3091', 'EC. DIFERENCIALES ORDINARIAS', 40),
+	('3697', 'COMPUTACION PARALELA', 20),
+	('3691', ' MODELOS DISC PARA ING SO', 25),
+	('3294', 'CÁLCULO VECTORIAL', 40),
+	('3700', 'PROGRAMACION WEB', 20);
+	
 select * from CURSO;
 
 
