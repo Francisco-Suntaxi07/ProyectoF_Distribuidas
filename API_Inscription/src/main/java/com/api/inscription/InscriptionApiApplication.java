e5f6e9818edb93793cf24312aadb422955f61445
Francisco Suntaxi <sfsuntaxi1@espe.edu.ec>
2024-03-12 18:58:23 -0500

    Crear api para matricular o inscripciones

---
+++ API_Inscription/src/main/java/com/api/inscription/InscriptionApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.inscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InscriptionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InscriptionApiApplication.class, args);
+	}
+
+}
