c20db52ff848c376d95e5aefde78fdc01fce0905
Francisco Suntaxi <sfsuntaxi1@espe.edu.ec>
2024-03-13 02:30:42 -0500

    Crear servicio para matricuñas

---
+++ sistema_academico_frontend/src/app/models/recordModel.ts
@@ -0,0 +1,11 @@
+export class RecordModel {
+    id?: string;
+    idUser?: string;
+    nameStudent?: string;
+    nameTeacher?: string;
+    nameCourse?: string;
+    gradeHomework?: number;
+    gradeProject?: number;
+    gradeExam?: number;
+    status?: boolean;
+}
---
+++ sistema_academico_frontend/src/app/services/record.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RecordModel } from '../models/recordModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecordService {
+
+  private urlEndPoint: string = 'http://localhost:8030/api/records';
+
+  constructor(private http:HttpClient) { }
+
+  public findAll(): Observable<RecordModel[]> {
+    return this.http.get<RecordModel[]>(`${this.urlEndPoint}/all`);
+  }
+
+  findById(id?: string): Observable<RecordModel> {
+    return this.http.get<RecordModel>(`${this.urlEndPoint}/${id}`);
+  }
+
+  save(record: RecordModel): Observable<any> {
+    return this.http.post<any>(`${this.urlEndPoint}/save`, record);
+  }
+
+  delete(id?: string): Observable<any> {
+    return this.http.delete<any>(`${this.urlEndPoint}/delete/${id}`);
+  }
+}
