fba26ec913f2b154e97577afe77323c191539354
Lusho24 <lxespinosa@espe.edu.ec>
2024-03-13 07:32:09 -0500

    .

--- sistema_academico_frontend/src/app/student/student-routing.module.ts
+++ sistema_academico_frontend/src/app/student/student-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationComponent } from './registration/registration.component';
-import { GradesStudentComponent } from './grades-student/grades-student.component';
+
 
 const routes: Routes = [
   { path: 'matriculas', component: RegistrationComponent },
-  { path: 'notas', component: GradesStudentComponent }
+
 ];
 
 @NgModule({
--- sistema_academico_frontend/src/app/student/student.module.ts
+++ sistema_academico_frontend/src/app/student/student.module.ts
@@ -8,12 +8,12 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
 
-
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    RegistrationComponent
+    RegistrationComponent,
+
 
   ],
   imports: [
@@ -22,7 +22,8 @@ import { MatOptionModule } from '@angular/material/core';
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
-    MatOptionModule
+    MatOptionModule,
+    FormsModule
 
   ]
 })
